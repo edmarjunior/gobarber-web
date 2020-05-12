1f7261155df4476835af28bf1fdff3faba9d1a38
Edmar Junior <edmarjunior17@gmail.com>
2020-05-12 15:54:06 -0300

    Criação do Header da aplicação

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Content, Profile } from './styles';
+import logo from '~/assets/logo-purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Go Barber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>Edmar Costa</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Edmar Costa"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
